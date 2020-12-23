a450d5a289a4edf79b73384de0e0699e9812df91
ayah24 <thinkpositiveayah@gmail.com>
2020-12-22 19:23:41 -0800

    h3

--- src/aboutme.js
+++ src/aboutme.js
@@ -5,6 +5,7 @@ function Resume() {
         <div>
             <h2>Ayah Aware</h2>
             <h3>Hello, This is my resume</h3>
+            <h3>Welcome to my page!</h3>
             
         </div>
     )
